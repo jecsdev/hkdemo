8a602cc02fd960d667d8c978ffa9b73ebafdb92b
john0 <jcampusano@k3d.do>
2022-05-12 15:52:21 -0400

    Working with ViewModel

--- app/src/main/java/com/hkfactorydemo/hkfactoryjohncampusano/ui/viewModels/PurchaseViewModel.kt
+++ app/src/main/java/com/hkfactorydemo/hkfactoryjohncampusano/ui/viewModels/PurchaseViewModel.kt
@@ -1,4 +1,26 @@
 package com.hkfactorydemo.hkfactoryjohncampusano.ui.viewModels
 
-class PurchaseViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.hkfactorydemo.hkfactoryjohncampusano.domain.GetPurchaseCase
+import com.hkfactorydemo.hkfactoryjohncampusano.domain.model.Purchase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class PurchaseViewModel @Inject constructor( private val getPurchaseCase: GetPurchaseCase):
+    ViewModel() {
+
+    private val purchaseModel = MutableLiveData<Purchase>()
+
+    fun onCreate(){
+        viewModelScope.launch {
+           val result = getPurchaseCase()
+            if(result.isNotEmpty()){
+                purchaseModel.value
+            }
+        }
+    }
 }
