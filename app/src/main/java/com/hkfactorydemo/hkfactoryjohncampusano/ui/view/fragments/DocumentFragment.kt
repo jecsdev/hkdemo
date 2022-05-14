05516a1173090b463a001908a16692f9b06a4f2b
john0 <jcampusano@k3d.do>
2022-05-13 20:49:52 -0400

    Making changes

--- app/src/main/java/com/hkfactorydemo/hkfactoryjohncampusano/ui/view/fragments/DocumentFragment.kt
+++ app/src/main/java/com/hkfactorydemo/hkfactoryjohncampusano/ui/view/fragments/DocumentFragment.kt
@@ -5,21 +5,40 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.fragment.app.commit
 import com.hkfactorydemo.hkfactoryjohncampusano.R
+import com.hkfactorydemo.hkfactoryjohncampusano.databinding.FragmentDocumentBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class DocumentFragment : Fragment() {
 
-
+    private lateinit var binding: FragmentDocumentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
         container?.removeAllViews()
-        return inflater.inflate(R.layout.fragment_document, container, false)
+        binding = FragmentDocumentBinding.inflate(inflater, container, false)
+        val bundle = Bundle()
+
+
+        binding.btnBackDispatch.setOnClickListener{
+            val customerFragment = CustomerFragment()
+            parentFragmentManager.commit {
+                setCustomAnimations(
+                    androidx.appcompat.R.anim.abc_slide_in_bottom,
+                    com.google.android.material.R.anim.abc_fade_out,
+                )
+                replace(R.id.documentFragment, customerFragment)
+            }
+        }
+        return binding.root
     }
 
 
