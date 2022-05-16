cc9d23ed894e25f7ccf69273ff8bea59b6f73a48
jcampusano <jcampusano@k3d.do>
2022-05-15 22:47:02 -0400

    Fixing details

--- app/src/main/java/com/hkfactorydemo/hkfactoryjohncampusano/ui/viewModels/PurchaseViewModel.kt
+++ app/src/main/java/com/hkfactorydemo/hkfactoryjohncampusano/ui/viewModels/PurchaseViewModel.kt
@@ -41,6 +41,7 @@ class PurchaseViewModel @Inject constructor(
     fun removePurchase( ){
         viewModelScope.launch {
             getPurchaseCase.deleteAllPurchases()
+            getPurchase()
         }
     }
 
